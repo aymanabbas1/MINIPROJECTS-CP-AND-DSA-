6ac82d2096e5d8cccc59a94c166cb5a2c3c25d58
aymanabbas1 <137698319+aymanabbas1@users.noreply.github.com>
2023-07-07 18:36:01 +0530

    Add files via upload

--- caeser-cipher/caeser-cipher.cpp
+++ caeser-cipher/caeser-cipher.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+// Function to encode the plaintext using Caesar cipher
+string encode(string plaintext, int shift) {
+    string text = "";
+
+    for (char ch : plaintext) {
+        if (isalpha(ch)) {
+            char base = isupper(ch) ? 'A' : 'a';
+            ch = (ch - base + shift) % 26 + base;
+        }
+        text += ch;
+    }
+
+    return text;
+}
+
+// Function to decode the ciphertext using Caesar cipher
+string decode(string ciphertext, int shift) {
+    return encode(ciphertext, 26 - shift);
+}
+
+int main() {
+    string plaintext;
+    int shift;
+    string process;
+
+    cout << "Enter 'encode' to encrypt and 'decode' to decrypt: ";
+    getline(cin, process);
+
+    cout << "Enter the message: ";
+    getline(cin, plaintext);
+
+    cout << "Enter shift value: ";
+    cin >> shift;
+
+    if (process == "encode") {
+        // Encode the plaintext
+        string ciphertext = encode(plaintext, shift);
+        cout << "The encoded message is: " << ciphertext << endl;
+    } else if (process == "decode") {
+        // Decode the ciphertext
+        string decryptedText = decode(plaintext, shift);
+        cout << "The decoded message is: " << decryptedText << endl;
+    } else {
+        cout << "Invalid process specified. Please enter 'encode' or 'decode'." << endl;
+    }
+
+    return 0;
+}
